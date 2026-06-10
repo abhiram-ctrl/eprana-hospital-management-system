3f971f34a869eab1f06791cfd75d6b69fd97b821
Abhiram <abhiramkov@gmail.com>
2026-06-11 02:55:37 +0530

    Configured production backend URL

--- eprana-frontend/src/api/axios.js
+++ eprana-frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://eprana-hospital-management-system.onrender.com",
 });
-
 API.interceptors.request.use((config) => {
 
   const token = localStorage.getItem("token");
